f839b57a77895d6c2a8b8a982245e40f2f83cb2d
gkvnoderr <123897793+gkvnoderr@users.noreply.github.com>
2023-02-19 19:34:22 +0200

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array=new int[n];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"array[{i}]");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = array[i] + array[0];
+            //    array[i] += array[0];
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            //Console.ReadKey();
+
+            //Console.Write("n=");
+            //int n=int.Parse(Console.ReadLine());
+            //int[]array = new int[n];
+            //Random rnd=new Random();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = rnd.Next(-10,10);
+            //    Console.WriteLine($"array[{i}]= {array[i]}");
+            //}
+            //int max = array[0];
+            //for (int i=1; i < array.Length; i++)
+            //{
+            //    if (array[i] > max)
+            //    {
+            //        max = array[i];
+
+            //    }
+            //}
+            //    Console.WriteLine($"max={max}");
+        }
+    }
+}
