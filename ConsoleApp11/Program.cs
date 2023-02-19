5d28baa83a1089ae18e143194ce318f9f8610630
gkvnoderr <123897793+gkvnoderr@users.noreply.github.com>
2023-02-19 19:45:39 +0200

    Add files via upload

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //задание 1
+            //Console.Write("размер=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"array[{i}]=");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //int p = array[0];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = array[i] + array[0];
+            //    //array[i] += array[0];
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            //Console.ReadKey();
+
+
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //Random rnd = new Random();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = rnd.Next(-10; 10);
+            //Console.WriteLine($"array [{i}]=array[i]}");
+
+
+            //задание 2
+            ////int[] array = { -7, -1, 2, -4, 8, -3 };
+            ////int count = 0;
+            ////foreach (int elem in array)
+            ////{
+            ////    if (elem % 2 == 0) count++;
+            ////}
+            ////Console.WriteLine($"count={count}");
+        }
+    }
+}
